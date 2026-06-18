72a2d0e143b82fe81c7527ae89f69fde794dc373
mounishr45 <mounishreddy64@gmail.com>
2026-06-18 11:22:01 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Career Progression and Promotion Gap Analysis")
+
+st.write("Project by Mounish Reddy")
+
+st.header("Project Overview")
+
+st.write("""
+This project analyzes employee career progression,
+promotion gaps, career stagnation and retention opportunities
+using machine learning and K-Means clustering.
+""")
+
+st.success("Project Successfully Deployed")
